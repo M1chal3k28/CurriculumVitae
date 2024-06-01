4f7d6603e09547f88a33dcd3e4eece38275ff251
M1chal3k28 <balcerakm0@gmail.com>
2024-06-01 16:06:50 +0200

    width and margin fixes

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -2,15 +2,15 @@ import profImg from "./assets/profiImg.jpg"
 
 function AboutMe() {
     return (
-        <section className="w-full h-full py-10 px-10">
-            <div className="flex md:flex-row flex-col bg-blue-200 py-2 px-2 rounded">
+        <section className="w-full h-full py-10 px-10 flex justify-center">
+            <div className="flex md:w-fit md:flex-row flex-col bg-blue-200 py-2 px-2 rounded">
                 {/* Image */}
-                <div className="md:py-4 md:px-5 flex items-center w-full justify-center m-auto">
+                <div className="md:py-4 md:px-5 md:w-46 flex items-center justify-center">
                     <img src={profImg} alt="Profile image" className="h-36 md:h-56 w-auto rounded"/>
                 </div>
 
                 {/* Text content */}
-                <div className="flex flex-col py-2 px-2 lg:text-2xl">
+                <div className="flex flex-col md:w-96 py-2 px-2 lg:text-2xl">
                     <div className="font-bold "><h1>About Me</h1></div>
                     <div></div>
                     {/* content */}
