6745c7d37d6ac16e81b181d71e427a3bd0800afd
M1chal3k28 <balcerakm0@gmail.com>
2024-06-04 23:35:02 +0200

    Underlined title of containers

--- src/Projects/ProjectsContainer.jsx
+++ src/Projects/ProjectsContainer.jsx
@@ -13,7 +13,7 @@ function ProjectsContainer() {
         <>
             <div className="px-4 sm:px-20 flex items-center justyfi-center py-10">
                 <div className="flex flex-col items-center m-auto justify-center">
-                    <h1 className="font-bold md:text-5xl text-2xl text-primary drop-shadow-lg">Finished/In Work Projects</h1>
+                    <h1 className="font-bold md:text-5xl text-2xl text-primary drop-shadow-lg"><span className="underline">Finished/In Progress</span> Projects</h1>
                     <ul className="m-auto w-auto grid xl:grid-cols-3 grid-cols-2 max-[767px]:grid-cols-1 md:gap-10 gap-1 py-6 bg-transparent ">
                         {projectCards}
                     </ul>
